5d357ecc3a254c18502378b57138005f5024dfc0
Satish Jaiswal <satish.131326.pr@gmail.com>
2018-02-24 21:40:25 +1100

    Error handling & messages, Service class made more concrete.

--- my-component-library/src/app/app.component.service.ts
+++ my-component-library/src/app/app.component.service.ts
@@ -3,19 +3,15 @@ import { Observable } from 'rxjs/Observable';
 import {Http,Response} from '@angular/http';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
-import * as moment from 'moment';
 import { IfDetails } from './app.component.if-details';
 
 @Injectable()
 export class SearchService implements OnInit{
-  private searchResult:IfDetails;
   private baseUrl:string="http://localhost:7878/first";
-  private searchResults:IfDetails[]=[];
-  private fareSearchResults = [];
 
   queryUrl: string = '?search=';
     //results: Object;
@@ -43,53 +39,16 @@ export class SearchService implements OnInit{
   getData():Observable <IfDetails[]>{
     return this.http.get(this.baseUrl)
     .map((res:Response)=><IfDetails[]>res.json())
+    .catch(this.errorHandler)
   }
 
-  getFlightsDetails(form:any):any[]
+  errorHandler(error)
   {
-    if(this.searchResults && this.searchResults.length>0)
-    {
-      this.searchResults=[];
-    }
-      this.getData().subscribe(data=>
-        {
-          console.log(data);
-          //data.filter(..) can also be used instead of loop
-          for (let i = 0; i < data.length; i++) 
-          {
-            //for now just to check.It will be replaced by actual queryString
-            let collectionDate: moment.Moment = moment(data[i].date);
-            let searchDate: moment.Moment = moment(form.dateInput.replace( /(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
-            
-            if ((data[i].from.toLowerCase( ) == form.from.toLowerCase( )) && 
-            (data[i].to.toLowerCase( ) == form.to.toLowerCase( )) &&  
-            (searchDate.format() == collectionDate.format()))  {
-                this.searchResult = data[i];
-                this.searchResults.push(this.searchResult);
-            }
-          }
-        })
-        console.log('Search Results: ',this.searchResults);
-        return this.searchResults;
-  }  
-  
-  getFareFilteredData(form:any):any[]
-  {
-    if(this.fareSearchResults.length!=0)
-    {
-      this.fareSearchResults=[];
-    }
-    for (let i = 0; i < this.searchResults.length; i++) 
-    {
-      //for now just to check.It will be replaced by actual queryString
-      if (Number(this.searchResults[i].fare) >=Number(form.fareFrom) 
-      && Number(this.searchResults[i].fare) <=Number(form.fareUpTo)  ) 
-      {
-          this.searchResult = this.searchResults[i];
-          this.fareSearchResults.push(this.searchResult);
-      }
-    }
-    console.log('Slider Filtered Results: ',this.fareSearchResults);
-    return this.fareSearchResults;
+    //logger can be used in real time app.
+    //for now
+    console.error("Something went wrong while fetching data from server");
+
+    //throw back the error.
+    return Observable.throw(error);
   }
 }
--- my-component-library/src/app/app.component.ts
+++ my-component-library/src/app/app.component.ts
@@ -10,9 +10,14 @@ import { IfDetails,IfDetailsHeader } from './app.component.if-details';
   providers:[SearchService]
 })
 export class AppComponent {
-  title = 'app'; 
-  flightsearchResult:IfDetails [];
+  private fareSearchResults = [];
+  private tempOriginalSearchResults = [];
+  private searchResult:IfDetails;
+
+  title = 'Flight Search Engine'; 
+  flightsearchResult:IfDetails []=[];
   currentSearchdetails:IfDetailsHeader={from:undefined,to:undefined,dateInput:undefined};
+  statusMessage:string
 
   constructor(private searchService: SearchService) {
     //this.searchService.search(this.searchTerm$)
@@ -28,15 +33,71 @@ export class AppComponent {
 
   onFlightSearchRequested(form:any):void{
     if(!form.filterRequested){   
-       this.flightsearchResult =   this.searchService.getFlightsDetails(form);
+       this.getFlightsDetails(form);
     }
     else {
-      this.flightsearchResult =   this.searchService.getFareFilteredData(form);
+      this.getFareFilteredData(form);
     }
 
     this.currentSearchdetails.from = form.from;
     this.currentSearchdetails.to = form.to;
     let searchDate: moment.Moment = moment(form.dateInput.replace( /(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
     this.currentSearchdetails.dateInput =searchDate.format();
+  }
+
+  
+  getFlightsDetails(form:any)
+  {
+    if(this.tempOriginalSearchResults && this.tempOriginalSearchResults.length>0)
+    {
+      this.tempOriginalSearchResults=[];
+      this.flightsearchResult=[];
+    }
+      this.searchService.getData().subscribe(data=>
+        {
+          console.log(data);
+          //data.filter(..) can also be used instead of loop
+          for (let i = 0; i < data.length; i++) 
+          {
+            //for now just to check.It will be replaced by actual queryString
+            let collectionDate: moment.Moment = moment(data[i].date);
+            let searchDate: moment.Moment = moment(form.dateInput.replace( /(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+            
+            if ((data[i].from.toLowerCase( ) == form.from.toLowerCase( )) && 
+            (data[i].to.toLowerCase( ) == form.to.toLowerCase( )) &&  
+            (searchDate.format() == collectionDate.format()))  {
+                this.searchResult = data[i];
+                this.flightsearchResult.push(this.searchResult);
+            }
+          }
+          //keep a copy for filter search
+          this.tempOriginalSearchResults= this.flightsearchResult
+        },
+        (error)=>{this.statusMessage='Ooops! Data fetching issues. Please try later.'},
+        ()=>{ this.flightsearchResult.length==0?this.statusMessage='Sorry! No Flights for the choosen criteria.':this.statusMessage=''; })
+        console.log('Search Results: ',this.flightsearchResult);
+  }  
+  
+  getFareFilteredData(form:any)
+  {
+    if(this.fareSearchResults.length!=0)
+    {
+      this.fareSearchResults=[];
+    }
+    for (let i = 0; i < this.tempOriginalSearchResults.length; i++) 
+    {
+      //for now just to check.It will be replaced by actual queryString
+      if (Number(this.tempOriginalSearchResults[i].fare) >=Number(form.fareFrom) 
+      && Number(this.tempOriginalSearchResults[i].fare) <=Number(form.fareUpTo)  ) 
+      {
+          this.searchResult = this.tempOriginalSearchResults[i];
+          this.fareSearchResults.push(this.searchResult);
+      }
+    }
+    console.log('Slider Filtered Results: ',this.fareSearchResults);
+    this.flightsearchResult = this.fareSearchResults;
+    this.flightsearchResult.length==0?
+      this.statusMessage='Sorry! No matching flight do exist. Please try with different fare range.':
+      this.statusMessage='';
   }
 }
